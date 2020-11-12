cdef19ada4999c4764e99698d508571d4f4507e6
Stefan Pratter <stefan@20c.com>
2020-11-12 15:09:57 +0000

    proxied api support

---
+++ src/django_devicectl/rest/urls/proxy.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from fullctl.django.rest.urls.proxy import setup, proxy_api
+
+PROXY_HOST = getattr(settings, "DEVICECTL_PROXY", None)
+ENDPOINTS = getattr(settings, "DEVICECTL_PROXY_ENDPOINTS", [
+    "device",
+    "phyport",
+    "virtport",
+    "logport",
+])
+
+if PROXY_HOST:
+
+    setup("devicectl", proxy_api("devicectl", PROXY_HOST, ENDPOINTS))
