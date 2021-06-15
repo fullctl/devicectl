633b78986a68bf4afb43c43c91fadef9be940906
Stefan Pratter <stefan@20c.com>
2021-06-15 15:15:25 +0000

    black format

--- src/django_devicectl/migrations/0002_virtualport_name.py
+++ src/django_devicectl/migrations/0002_virtualport_name.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_devicectl', '0001_initial'),
+        ("django_devicectl", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='virtualport',
-            name='name',
+            model_name="virtualport",
+            name="name",
             field=models.CharField(blank=True, max_length=255),
         ),
     ]
