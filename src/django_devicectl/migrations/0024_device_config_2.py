6c412eb80e3f3dc4734c382ebda83a0b3339bd00
Matt Griswold <grizz@20c.com>
2023-05-15 11:38:09 +0000

    linting

--- src/django_devicectl/migrations/0024_device_config_2.py
+++ src/django_devicectl/migrations/0024_device_config_2.py
@@ -4,38 +4,45 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('django_devicectl', '0023_device_dconfig'),
+        ("django_devicectl", "0023_device_dconfig"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='deviceconfighistory',
-            name='diff',
+            model_name="deviceconfighistory",
+            name="diff",
         ),
         migrations.RemoveField(
-            model_name='deviceoperationalstatus',
-            name='diff',
+            model_name="deviceoperationalstatus",
+            name="diff",
         ),
         migrations.AddField(
-            model_name='deviceconfighistory',
-            name='config_current',
-            field=models.TextField(blank=True, help_text='Current config contents', null=True),
+            model_name="deviceconfighistory",
+            name="config_current",
+            field=models.TextField(
+                blank=True, help_text="Current config contents", null=True
+            ),
         ),
         migrations.AddField(
-            model_name='deviceconfighistory',
-            name='config_reference',
-            field=models.TextField(blank=True, help_text='Reference config contents', null=True),
+            model_name="deviceconfighistory",
+            name="config_reference",
+            field=models.TextField(
+                blank=True, help_text="Reference config contents", null=True
+            ),
         ),
         migrations.AddField(
-            model_name='deviceoperationalstatus',
-            name='config_current',
-            field=models.TextField(blank=True, help_text='Current config contents', null=True),
+            model_name="deviceoperationalstatus",
+            name="config_current",
+            field=models.TextField(
+                blank=True, help_text="Current config contents", null=True
+            ),
         ),
         migrations.AddField(
-            model_name='deviceoperationalstatus',
-            name='config_reference',
-            field=models.TextField(blank=True, help_text='Reference config contents', null=True),
+            model_name="deviceoperationalstatus",
+            name="config_reference",
+            field=models.TextField(
+                blank=True, help_text="Reference config contents", null=True
+            ),
         ),
     ]
