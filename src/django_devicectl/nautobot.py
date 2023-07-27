16139a9954dcea3ce795f7bcf056816c8eaa6cf7
Stefan Pratter <stefan@20c.com>
2023-07-27 14:42:15 +0000

    remove debug message

--- src/django_devicectl/nautobot.py
+++ src/django_devicectl/nautobot.py
@@ -311,9 +311,6 @@ def pull(org, *args, **kwargs):
             platform = nautobot_device.platform.slug
             if platform != device.meta.get("platform"):
                 device.meta["platform"] = platform
-                print(
-                    f"[PULL] Nautobot device {nautobot_device.name} platform {platform}"
-                )
                 changed = True
 
         if changed or created:
